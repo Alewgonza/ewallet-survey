1698005dc36b0e9002fbd7a5d9d45cead05e64c8
alewgonza <alewgonza@gmail.com>
2023-08-03 19:42:17 -0300

    Initial commit publish project

---
+++ src/test/java/com/ewalletsurvey/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ewalletsurvey.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
